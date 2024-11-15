c0d2ac0213d0bf51bd80c1969db72293cae13fff
VaishnaviPolakonda <polakondavaishnavi2004@gmail.com>
2024-11-15 10:32:54 +0530

    Create Season Game.py

---
+++ visa_prep/Season Game.py
@@ -0,0 +1,11 @@
+a=int(input())
+if 3<=a<=5:
+    print("Spring")
+elif 6<=a<=8:
+    print("Summer")
+elif 9<=a<=11:
+    print("Autumn")
+elif a==12 or 1<=a<=2:
+    print("Winter")
+else:
+    print("Invalid")
