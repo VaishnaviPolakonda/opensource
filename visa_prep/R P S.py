4ed1a6d55f11cefca11ea87c0f047e230c46c953
VaishnaviPolakonda <polakondavaishnavi2004@gmail.com>
2024-11-17 19:20:41 +0530

    Delete visa_prep/R P S.py

--- visa_prep/R P S.py
+++
@@ -1,7 +0,0 @@
-v,c=input().split()
-if (v=='R' and c=='S') or (v=='S' and c=='P') or (v=='P' and c=='R') :
-    print("Vignesh")
-elif(v=='R' and v=='S') or (c=='P' and v=='R') or (c=='S' and v=='P'):
-    print("Charan")
-else:
-    print("NULL")
